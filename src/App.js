72c75ff2a67b04634d0875ca55da3c7849975f7c
Gideon Mensa <gideon123ewq@gmail.com>
2020-04-26 21:05:14 +0300

    Added Gratitude component

--- src/App.js
+++ src/App.js
@@ -16,10 +16,11 @@ import { Callback } from "./utils/callback/callback";
 import AuthCheck from "./utils/authcheck";
 import Auth from "./utils/auth";
 import history from "./utils/history/history";
+import Gratitude from "./components/userFunction";
 
 const styles = {
-  nav: {
-    marginRight: "5em",
+  RouteComponent: {
+    marginTop: "5em",
   },
 };
 const ProtectedRoute = (props) => {
@@ -43,54 +44,59 @@ function App(props) {
   return (
     <Router history={history}>
       <div className="header">
-        <Navbar bg="light" expand="lg">
-          <Navbar.Brand>Car2GO</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Link to="/" className="nav-link">
-                Home
-              </Link>
-            </Nav>
-            <Nav style={styles.nav}>
-              <NavDropdown title="More" id="basic-nav-dropdown">
-                <NavDropdown.Item>
-                  <Link to="/return-car">Return car</Link>
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item>
-                  <Link to="/dashboard/login">Dashboard</Link>
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/user/rent" component={Rental} />
-          <Route path="/return-car" component={ReturnCar} />
-          <Route
-            path="/dashboard/logIn"
-            exact
-            render={(props) => <LogIn auth={auth} />}
-          />
-          <Route exact path="/rent-car/:type/:size" component={CarRent} />
-          <Route path="/car2go-payment/:carId" component={PaymentPage} />
-          <Route
-            path="/callback"
-            render={(props) => {
-              handleAuthentication(props);
-              return <Callback history={history} />;
-            }}
-          />
-          <Route
-            path="/authcheck"
-            props={props.AuthReducer}
-            component={AuthCheck}
-          />
-          <ProtectedRoute props={AuthReducer} />
-          {/* <Route path="/car-detail/:carNumber" component={CarDetail} /> */}
-        </Switch>
+        <nav className="navbar navbar-expand-lg fixed-top navbar-light bg-light">
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <Link className="nav-link" to="/">
+                  Home <span className="sr-only">(current)</span>
+                </Link>
+              </li>
+            </ul>
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/return-car">
+                  Return car
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/dashboard/logIn">
+                  Dashboard
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <div style={styles.RouteComponent}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/user/rent" component={Rental} />
+            <Route path="/return-car" component={ReturnCar} />
+            <Route
+              path="/dashboard/logIn"
+              exact
+              render={(props) => <LogIn auth={auth} />}
+            />
+            <Route exact path="/rent-car/:type/:size" component={CarRent} />
+            <Route path="/car2go-payment/:carId" component={PaymentPage} />
+            <Route
+              path="/callback"
+              render={(props) => {
+                handleAuthentication(props);
+                return <Callback history={history} />;
+              }}
+            />
+            <Route path="/thank-you-user" component={Gratitude} />
+            <Route
+              path="/authcheck"
+              props={props.AuthReducer}
+              component={AuthCheck}
+            />
+            <ProtectedRoute props={AuthReducer} />
+
+            {/* <Route path="/car-detail/:carNumber" component={CarDetail} /> */}
+          </Switch>
+        </div>
       </div>
     </Router>
   );
