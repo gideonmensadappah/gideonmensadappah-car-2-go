7b56beefa1d228e21456e833bae1a113743af3b8
Gideon Mensa <gideon123ewq@gmail.com>
2020-04-26 22:20:54 +0300

    Fixed Router: On Log Out The User Is Directed To Home Page

--- src/components/dashboard/dashboard-profile.jsx
+++ src/components/dashboard/dashboard-profile.jsx
@@ -4,10 +4,11 @@ import SideNav from "./dashboard-side-nav";
 import { login_failure } from "../../actions/action_types";
 import RentedCars from "./dashboard_comps/rented_cars";
 import Inventory from "./dashboard_comps/inventory";
-import { Customers } from "./dashboard_comps/customers";
+import Customers from "./dashboard_comps/customers";
 import { selectRentedCars } from "../../reducers/reducers";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import AddNewCar from "../dashboard/add-New-Car";
+
 const styles = {
   container: {
     marginLeft: "13em",
@@ -17,16 +18,16 @@ const Dashboard = ({ logOut }) => {
   return (
     <Router>
       <SideNav logOut={logOut} />
-      <Switch>
-        <div className="container" style={styles.container}>
-          <div className="row">
+      <div className="container" style={styles.container}>
+        <div className="row">
+          <Switch>
             <Route path="/dashboard/rented-cars" component={RentedCars} />
             <Route path="/dashboard/inventory" component={Inventory} />
             <Route path="/dashboard/customers" component={Customers} />
             <Route path="/dashboard/new-car" component={AddNewCar} />
-          </div>
+          </Switch>
         </div>
-      </Switch>
+      </div>
     </Router>
   );
 };
@@ -36,9 +37,5 @@ const mapStateToProps = (storeState) => {
     rentedCars: selectRentedCars(storeState),
   };
 };
-const mapDispatchToProps = (dispatch) => {
-  return {
-    logOut: () => dispatch(login_failure),
-  };
-};
-export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
+
+export default connect(mapStateToProps)(Dashboard);
