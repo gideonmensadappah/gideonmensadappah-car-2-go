ad0a2f368a52ceed62672a3d31b289b75008d3b6
Gideon Mensa <gideon123ewq@gmail.com>
2020-04-26 21:09:47 +0300

    ADDED Action Types: ADD COSTUMER REMOVE COSTUMER

--- src/actions/action_types.js
+++ src/actions/action_types.js
@@ -1,10 +1,21 @@
 export const ActionType = {
+  ADD_COSTOMER: "ADD_COSTOMER",
+  REMOVE_COSTOMER: "REMOVE_COSTOMER",
   LOGIN_SUCCESS: "LOGIN_SUCCESS",
   LOGIN_FAILURE: "LOGIN_FAILURE",
   RENT_CAR: "RENT_CAR",
   RETURN_CAR: "RETURN_CAR",
   ADD_NEW_CAR: "ADD_NEW_CAR",
 };
+
+export const addCostomer = (payload) => ({
+  type: ActionType.ADD_COSTOMER,
+  payload,
+});
+export const removeCustomer = (carNumber) => ({
+  type: ActionType.REMOVE_COSTOMER,
+  id: carNumber,
+});
 
 export const rent = (id) => ({
   type: ActionType.RENT_CAR,
