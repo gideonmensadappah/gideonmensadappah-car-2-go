5e8a4d5aed9fd92a34b6250bef4e99e9871d7f18
Gideon Mensa <gideon123ewq@gmail.com>
2020-05-01 22:38:59 +0300

    Removed Unnecessary div

--- src/App.js
+++ src/App.js
@@ -45,61 +45,80 @@ function App(props) {
   const { isAuthenticated } = props.auth;
 
   return (
-    <Router history={history}>
-      <div className="header">
-        <nav className="navbar navbar-expand-lg fixed-top navbar-light bg-light">
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item active">
-                <Link className="nav-link" to="/">
-                  Home <span className="sr-only">(current)</span>
-                </Link>
-              </li>
-            </ul>
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/return-car">
-                  Return car
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/dashboard/logIn">
-                  Dashboard
-                </Link>
-              </li>
-            </ul>
+    <>
+      <Router history={history}>
+        <div className="header">
+          <nav className="navbar navbar-expand-lg bg-inverse  fixed-top navbar-light bg-light">
+            <div className="navbar-header">
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+                onClick={() => alert("hello")}
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+            </div>
+            <div
+              className="collapse navbar-collapse navbar-toggleable-xs"
+              id="navbarNav"
+            >
+              <ul className="navbar-nav">
+                <li className="nav-item active">
+                  <Link className="nav-link" to="/">
+                    Home <span className="sr-only">(current)</span>
+                  </Link>
+                </li>
+              </ul>
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/return-car">
+                    Return car
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/dashboard/logIn">
+                    Dashboard
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <div style={styles.RouteComponent}>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/user/rent" component={Rental} />
+              <Route path="/return-car" component={ReturnCar} />
+              <Route
+                path="/dashboard/logIn"
+                exact
+                render={(props) => <LogIn auth={auth} />}
+              />
+              <Route exact path="/rent-car" component={CarRent} />
+              <Route path="/car2go-payment/:carId" component={PaymentPage} />
+              <Route
+                path="/callback"
+                render={(props) => {
+                  handleAuthentication(props);
+                  return <Callback history={history} />;
+                }}
+              />
+              <Route path="/thank-you-user" component={Gratitude} />
+              <Route
+                path="/authcheck"
+                props={props.AuthReducer}
+                component={AuthCheck}
+              />
+              <ProtectedRoute isAuthenticated={isAuthenticated} />
+            </Switch>
           </div>
-        </nav>
-        <div style={styles.RouteComponent}>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/user/rent" component={Rental} />
-            <Route path="/return-car" component={ReturnCar} />
-            <Route
-              path="/dashboard/logIn"
-              exact
-              render={(props) => <LogIn auth={auth} />}
-            />
-            <Route exact path="/rent-car" component={CarRent} />
-            <Route path="/car2go-payment/:carId" component={PaymentPage} />
-            <Route
-              path="/callback"
-              render={(props) => {
-                handleAuthentication(props);
-                return <Callback history={history} />;
-              }}
-            />
-            <Route path="/thank-you-user" component={Gratitude} />
-            <Route
-              path="/authcheck"
-              props={props.AuthReducer}
-              component={AuthCheck}
-            />
-            <ProtectedRoute isAuthenticated={isAuthenticated} />
-          </Switch>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </>
   );
 }
 const mapStateToProps = (state) => ({
