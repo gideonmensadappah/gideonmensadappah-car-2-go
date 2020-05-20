dbaf68e81c1237ded3c68d86b07ac1fd684dc603
Gideon Mensa <gideon123ewq@gmail.com>
2020-05-20 10:32:05 +0300

    extracted topNav to different Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React from "react";
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  Redirect,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { connect } from "react-redux";
 import Home from "../src/components/home";
 import CarRent from "../src/components/car-rent";
@@ -20,7 +14,7 @@ import AuthCheck from "./utils/authcheck";
 import Auth from "./utils/auth";
 import history from "./utils/history/history";
 import Gratitude from "./components/gratitude";
-
+import TopNav from "./components/topNav/topNav";
 const styles = {
   RouteComponent: {
     marginTop: "5em",
@@ -48,46 +42,7 @@ function App(props) {
     <>
       <Router history={history}>
         <div className="header">
-          <nav className="navbar navbar-expand-lg bg-inverse  fixed-top navbar-light bg-light">
-            <div className="navbar-header">
-              <button
-                className="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarNav"
-                aria-controls="navbarNav"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-                onClick={() => alert("hello")}
-              >
-                <span className="navbar-toggler-icon"></span>
-              </button>
-            </div>
-            <div
-              className="collapse navbar-collapse navbar-toggleable-xs"
-              id="navbarNav"
-            >
-              <ul className="navbar-nav">
-                <li className="nav-item active">
-                  <Link className="nav-link" to="/">
-                    Home <span className="sr-only">(current)</span>
-                  </Link>
-                </li>
-              </ul>
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" to="/return-car">
-                    Return car
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/dashboard/logIn">
-                    Dashboard
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
+          <TopNav adminIsConnected={isAuthenticated} />
           <div style={styles.RouteComponent}>
             <Switch>
               <Route exact path="/" component={Home} />
